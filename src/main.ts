a3743bf4ad8ce6606a12d76175be8c1a83f8b5c0
hoangday185 <jarvietnguyen318@gmail.com>
2025-02-06 15:10:10 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const corsOption = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  };
+  app.enableCors(corsOption);
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
